2cfc0745b0243ec4adce8eec30aedd56be4295e6
Daedra <foglzerika@gmail.com>
2020-07-23 08:08:15 -0500

    feat | Adding code splitting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useState , lazy , Suspense} from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+//import Details from "./Details";
+//import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
+
+
+const Details = lazy(() => import("./Details"))
+const SearchParams = lazy ( () => import ("./SearchParams"))
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
        <NavBar/>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+       <Suspense fallback={ <h1>loading route..</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+       </Suspense>
       </div>
     </ThemeContext.Provider>
   );
