f508f58d39c341837e463c78b54d5a4cbced929b
Daedra <foglzerika@gmail.com>
2020-07-22 19:32:25 -0500

    feat | Adding click event to navbar component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link} from "@reach/router"
 import {css} from "@emotion/core"
 
 const colorBlue = "aqua"
 
-const NavBar = () =>(
-  <header css={ css`
-  background-color: ${colorBlue};
-  padding: 3em;
-`} >
-    <Link to="/">
-      Adopt me
-    </Link>
-    <span role="img" aria-label="logo">🐶🐶🐶🐶🐾</span>
-  </header>
-)
+const NavBar = () =>{
+
+  const [padding, setPadding] = useState(3);
+  return(
+    <header
+      onClick={() => setPadding(padding+5)}
+      css={ css`
+      background-color: ${colorBlue};
+      padding: ${padding}em;
+      `} >
+      <Link to="/">
+        Adopt me
+      </Link>
+      <span role="img" aria-label="logo">🐶🐶🐶🐶🐾</span>
+    </header>
+  )
+}
+
+
 export default  NavBar;
